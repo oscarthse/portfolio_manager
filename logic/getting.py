174f57908327e360b78fd0193cea28b5e1236665
Oscar Thiele <oscarthse@gmail.com>
2024-09-02 10:06:34 +0200

    getting models from bucket

--- logic/getting.py
+++ logic/getting.py
@@ -1,6 +1,9 @@
 import os
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
 import pandas as pd
+import tensorflow as tf
+import tempfile
+import io
 
 def get_price_data_raw(ticker):
     client = bigquery.Client(project="portfolio-manager-433711")
@@ -37,3 +40,19 @@ def get_macro_data():
     df = client.query(query).to_dataframe()
 
     return df
+
+def get_neural_nets(ticker):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('model-bucket-portfolio')
+    blob_name = f'model_{ticker}.keras'
+    blob = bucket.blob(blob_name)
+    model_bytes = blob.download_as_bytes()
+
+    # Create a temporary file to store the model bytes
+    with tempfile.NamedTemporaryFile(suffix='.keras') as temp_model_file:
+        temp_model_file.write(model_bytes)
+        temp_model_file.flush()  # Ensure all data is written
+
+        # Load the model from the tempora file
+        model = tf.keras.models.load_model(temp_model_file.name)
+    return model
